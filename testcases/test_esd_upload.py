7306253fcbd453fba456e05aa445a07272d1a8f0
Harshali <106228538+PreshitaJambhale@users.noreply.github.com>
2026-05-25 12:49:16 +0530

    Update test_esd_upload.py
    logs added

--- testcases/test_esd_upload.py
+++ testcases/test_esd_upload.py
@@ -3,17 +3,26 @@ import pytest
 from base.Base_file1 import BaseDriver
 from pages.esd_upload_page import EsdUpload
 from utilities.read_config import ReadConfigfiles
+import logging
+
 
 
 @pytest.mark.usefixtures("setup")
 class Test_EsdUpload():
 
     def test_esd_upload(self):
+        logging.basicConfig(level=logging.INFO)
+
+
+
         bd = BaseDriver(self.driver)
         ed = EsdUpload(self.driver)
         bd.open_url(ReadConfigfiles.get_esd_upload())
         bd.login(ReadConfigfiles.get_username(), ReadConfigfiles.get_password())
-        ed.search_and_upload("STG-PO-2027-0076-R1")
+        logging.info("Browser launched")
+        logging.info("Login successful")
+        ed.search_and_upload("STG-PO-2027-0082")
+        logging.info("Upload page opened")
         time.sleep(2)
         current_url = self.driver.current_url
         assert current_url == ReadConfigfiles.get_esd_attach(), "Attachment page does not open."
@@ -21,6 +30,7 @@ class Test_EsdUpload():
         ed.apply_to_all("Both (Internal & Customer)")
         time.sleep(2)
         current_url = self.driver.current_url
+        logging.info("Upload successful")
         assert current_url == ReadConfigfiles.get_esd_search(), "Document did not get uploaded."
 
 
